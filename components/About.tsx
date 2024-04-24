813d8e3006965eacf25a958635875367ff4ca555
Nathan Rydel <nathanrydel@gmail.com>
2024-04-23 20:17:27 -0700

    update animations

--- components/About.tsx
+++ components/About.tsx
@@ -13,8 +13,12 @@ export default function About() {
       ref={ref}
       className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.175 }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+      }}
+      transition={{ delay: 0.2 }}
+      viewport={{ once: true}}
       id="about"
     >
       <SectionHeading>About me</SectionHeading>
