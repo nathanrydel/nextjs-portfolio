514ea7ba6e435454fdbb9d5b86b221dab8d203ba
Nathan Rydel <nathanrydel@gmail.com>
2024-04-23 20:15:27 -0700

    rename pattern match

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import SectionHeading from "./SectionHeading";
-import { skillsData } from "@/lib/data";
+import { SKILLS } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { motion } from "framer-motion";
 
@@ -44,7 +44,7 @@ export default function Skills() {
     >
       <SectionHeading>My skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-        {skillsData.map((skill, index) => (
+        {SKILLS.map((skill, index) => (
           <motion.li
             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
             key={index}
