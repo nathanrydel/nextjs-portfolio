d2854b74806c9a7bf35102a7a8adf9a6c9e90aad
Nathan Rydel <nathanrydel@gmail.com>
2024-04-23 22:28:42 -0700

    Create <Contact>
    - create and style
    - added to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Experience from "@/components/Experience";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
@@ -17,6 +19,8 @@ export default function Home() {
       <Projects />
       <Skills />
       <About />
+      <Experience />
+      <Contact />
     </main>
   );
 }
