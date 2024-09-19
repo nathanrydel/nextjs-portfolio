5be570d569137668ed45f2c80c7541e1e58fa710
Nathan Rydel <nathanrydel@gmail.com>
2024-09-19 14:41:50 -0700

    Add speed insights from vercel to main

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
         <Header />
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
