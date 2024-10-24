80ec36e7250069e240a0bdf38a2f5bcffd96a181
Nathan Rydel <nathan@nathanrydel.dev>
2024-10-23 20:27:53 -0700

    update sendEmail

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -36,7 +36,7 @@ export const sendEmail = async (formData: FormData) => {
       from: "Contact Form <onboarding@resend.dev>",
       to: "contact@nathanrydel.dev",
       subject: "Message from contact form",
-      reply_to: senderEmail,
+      replyTo: senderEmail,
       react: React.createElement(ContactFormEmail, {
         message: message,
         senderEmail: senderEmail,
