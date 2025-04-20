216bf4855372e3279550743ce4a8c294a15a96a2
Nathan Rydel <43457298+nathanrydel@users.noreply.github.com>
2025-04-20 11:46:24 -0700

    About Updates

--- components/About.tsx
+++ components/About.tsx
@@ -34,26 +34,26 @@ export default function About() {
 
       <ul className="mb-5 text-left text-lg list-disc list-inside">
         <li>
-          <span className="font-medium">AI-integrated web apps</span> (GripLab AI, Python/TypeScript)
+          <span className="font-medium">AI-integrated web apps</span> (ChatGPT | Claude | N8N, Python/TypeScript)
         </li>
         <li>
-          <span className="font-medium">Scalable backends</span> (REST APIs, Go, C#, SQL)
+          <span className="font-medium">Scalable backends</span> (REST APIs, Go, Python, C#, SQL)
         </li>
         <li>
-          <span className="font-medium">DevOps pipelines</span> (AWS, Docker, CI/CD)
+          <span className="font-medium">DevOps pipelines</span> (AWS, Azure, Docker, CI/CD)
         </li>
       </ul>
 
-      <p className="mb-3">
-        Previously <span className="font-medium">7+ years in technical support</span> (Apple, Accenture), giving me a sharp eye for <span className="font-medium">debugging, performance tuning, and user-centric design</span>.
-      </p>
+      <h3 className="mb-3 text-xl">
+        Previously <span className="font-medium">7+ years in technical and developer relations support</span> (Apple, Accenture), giving me a sharp eye for <span className="font-medium">debugging, performance tuning, and user-centric design</span>.
+      </h3>
 
       <p className="font-medium mb-1 mt-6">What I Do</p>
       <ul className="text-left list-disc list-inside grid grid-cols-1 sm:grid-cols-2 gap-x-4">
         <li><span className="font-medium">Front-End:</span> React, Vue, Next.js, TailwindCSS</li>
-        <li><span className="font-medium">Back-End:</span> Python, Go, Node.js, RESTful APIs</li>
+        <li><span className="font-medium">Back-End:</span> Python, Go, C#, Node.js, RESTful APIs</li>
         <li><span className="font-medium">DevOps & Cloud:</span> AWS, Azure, Docker, CI/CD</li>
-        <li><span className="font-medium">AI/ML:</span> LLM integrations, data analytics models</li>
+        <li><span className="font-medium">AI/ML:</span> LLM integrations, AI Agent, RAG</li>
       </ul>
     </motion.section>
   );
